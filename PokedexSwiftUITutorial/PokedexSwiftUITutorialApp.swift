20e22eaca7869e1de9a3ba672530fe39dc26ecbd
Berat Yavuz <beratttyavuzz@gmail.com>
2022-05-13 19:13:06 +0300

    Initial Commit

---
+++ PokedexSwiftUITutorial/PokedexSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUITutorialApp.swift
+//  PokedexSwiftUITutorial
+//
+//  Created by Berat Yavuz on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
